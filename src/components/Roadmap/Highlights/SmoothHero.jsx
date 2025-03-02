3e3a715cd42657b22371405d519fb4df77172e94
Lefika <lefikatmoalosi@gmail.com>
2025-03-02 01:51:40 -0500

    fixed the spacing of the h1 heading on the roadmap page

--- src/components/Roadmap/Highlights/SmoothHero.jsx
+++ src/components/Roadmap/Highlights/SmoothHero.jsx
@@ -34,7 +34,7 @@ const Schedule = () => {
         initial={{ y: 48, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ ease: "easeInOut", duration: 0.75 }}
-        className="mb-20 text-4xl font-black uppercase text-zinc-50 mt-[200px]"
+        className="mb-15 text-4xl font-black uppercase text-zinc-50 px-3 mt-[130px]"
       >
         Launch Schedule
       </motion.h1>
