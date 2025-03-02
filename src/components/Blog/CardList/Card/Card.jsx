638b7f9c7c4316451ee7eaf07096101dc028bb57
Lefika <lefikatmoalosi@gmail.com>
2025-03-01 19:23:02 -0500

    made blog responsive

--- src/components/Blog/CardList/Card/Card.jsx
+++ src/components/Blog/CardList/Card/Card.jsx
@@ -5,14 +5,14 @@ import { posts } from "../../../../assets/Blog";
 const Card = () => {
 
   return (
-    <div className="flex flex-wrap gap-[30px]">
+    <div className="flex flex-wrap gap-[10px] sm: gap-[10px] lg:gap-[10px] xl:gap-[10px]">
         {posts.map(post => (
             <div key={post.slug} className="container relative flex sm:flex-col lg:flex-row xl:flex-row item-center 
                                             gap-[30px] sm:mb-[30px] sm:text-center">
-                <div className="flex-1 h-[250px] relative sm:flex sm:justify-center md:flex md:justify-center lg:block xl:block">
-                    <img src={post.image} className="rounded-[0.5rem] w-full h-[15rem] object-cover" width={500} height={500} alt=""/>
+                <div className="flex-1 h-[250px] relative sm:flex-row sm:justify-center md:flex md:justify-center lg:block xl:block">
+                    <img src={post.image} className="rounded-[0.5rem] w-full h-[15rem] sm: h-[14rem] lg:h-[15rem] object-cover" width={500} height={500} alt=""/>
                 </div>
-                <div className="flex-1 flex flex-col gap-[10px] lg:text-left xl:text-left">
+                <div className="flex-1 flex flex-col gap-[10px] text-center lg:text-left xl:text-left">
                     <div className="">
                         <span className="text-gray-500">{post.date} - </span>
                         <span className="text-pink-600 font-[600]">{post.category}</span>
@@ -21,9 +21,7 @@ const Card = () => {
                         <h1 className="text-xl font-bold">{post.title}</h1>
                     </Link>
                     <p className="font-light text-[12px] text-gray-400">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae 
-                        velit consectetur, dolorum fugit impedit saepe deleniti aliquam! 
-                        Quidem repellat?...
+                       {post.description}
                     </p>
                     <div className="flex justify-center lg:justify-start xl:justify-start">
                         <Link to={`/post/${post.slug}`} className="border-b-2 border-gray-400 text-[12px] w-max hover:border-gray-600 transition-all">
